865feabbced590039fc4d59faac93d7d9951a842
JohnlyndelCastillo <johnlyndelcastillo@gmail.com>
2026-05-09 22:46:12 +0800

    chore: code cleanup

--- dashboard/admin/dashboard.php
+++ dashboard/admin/dashboard.php
@@ -18,9 +18,6 @@ $result = $stmt->get_result();
   <title>Admin Dashboard</title>
   <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/public/css/common.css?v=1.0">
-  <link rel="stylesheet" href="/public/css/approval-badge/admin_role_badge_style.css?v=1.0">
-  <link rel="stylesheet" href="/public/css/form/form_common_style.css?v=1.0">
-  <link rel="stylesheet" href="/public/css/modal/modal_component_style.css?v=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
     integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
